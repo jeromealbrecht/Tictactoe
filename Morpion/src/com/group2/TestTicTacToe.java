e6ae5eaf56a376516c56494f7a3be08173ac9d8e
alicia bazin <bazin.alicia88@gmail.com>
2024-07-19 13:13:53 +0200

    add method java

---
+++ Morpion/src/com/group2/TestTicTacToe.java
@@ -0,0 +1,9 @@
+package com.group2;
+
+public class TestTicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ Morpion/src/com/group2/TicTacToe.java
@@ -0,0 +1,35 @@
+package com.group2;
+
+public class TicTacToe {
+	// TODO
+	public static void displayGameOver() {
+
+	}
+
+// TODO 
+	public static void displayRules() {
+
+	}
+
+	// TODO ZEDOUN
+	public static void diplayGrid() {
+
+	}
+
+	// TODO ZEDOUN
+	public static void displayWelcome() {
+
+	}
+
+	// TODO JEROME
+	public static int[] chooseCase() {
+		int[] cases = { 0, 0 };
+		return cases;
+	}
+
+	// TODO ALICIA
+	public static boolean game() {
+		boolean isWin = true;
+		return isWin;
+	}
+}
